f38afc1a7ea7f2660e3acd98b79dfcd7a163c180
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-04-25 22:20:16 +0900

    Feat : 계산기 구현 완료

--- 02. 계산기/highOrderFunction.js
+++ 02. 계산기/highOrderFunction.js
@@ -1,5 +1,5 @@
-let a = '';
-if (!a) {
+let a1 = '1';
+if (a1) {
     console.log('true');
 } else {
     console.log('false');
@@ -35,3 +35,33 @@ const second = first(4);
 const third = second(5);
 
 console.log(third);
+
+// if 중첩 줄이기
+function test() {
+    let result = '';
+
+    if (a) {
+        if (!b) {
+            result = 'c';
+        }
+    } else {
+        result = 'a';
+    }
+    result += 'b';
+    return result;
+}
+
+function testResult() {
+    let result = '';
+
+    if (!a) {
+        result = 'a';
+        result += 'b';
+        return result;
+    }
+    if (!b) {
+        result = 'c';
+    }
+    result += 'b';
+    return result;
+}
