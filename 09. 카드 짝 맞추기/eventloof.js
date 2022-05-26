e2fd538aa28cf79d575277cc265c94573610a74a
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-05-26 20:12:36 +0900

    Feat : 이벤트 루프 코드 분석'

--- 09. 카드 짝 맞추기/eventloof.js
+++ 09. 카드 짝 맞추기/eventloof.js
@@ -21,3 +21,21 @@ function startFun() {
 }
 
 start();
+
+
+function aaa() {
+    setTimeout(() => {
+        console.log('d');
+    }, 0);
+    console.log('c');
+}
+
+setTimeout(() => {
+    console.log('a');
+    aaa();
+}, 0);
+
+setTimeout(() => {
+    aaa();
+    console.log('b');
+}, 0);
